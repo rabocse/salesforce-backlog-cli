56e5956f13c0dc23a538a633db28e8d06bf99ad8
Alex Rabocse <alexdescobars@gmail.com>
2022-10-03 17:18:54 +0200

    Added commented out section to be implemented later on

--- cli/cli.go
+++ cli/cli.go
@@ -1,4 +1,4 @@
-package main
+package cli
 
 import (
 	"flag"
@@ -23,7 +23,7 @@ subcommand 'lv'
 
 */
 
-func main() {
+func Cli() {
 
 	sr := flag.NewFlagSet("sr", flag.ExitOnError)
 	n := sr.String("n", "", "Service Request Number")
@@ -48,7 +48,22 @@ func main() {
 		lv.Parse(os.Args[2:])
 		fmt.Println("subcommand 'lv'")
 		fmt.Println("  listview:", *l)
-		fmt.Println("  tail:", sr.Args())
+		// //fmt.Println("  tail:", sr.Args())
+
+		// // Building the URL to query the data.
+		// casesURL := sftool.BuildURL(SfCred.SalesforceInstance, 2)
+
+		// // Crafting a valid HTTPS request with TLS ignore.
+		// casesReq := sftool.CraftRequest(http.MethodGet, casesURL, accessToken, nil)
+
+		// // Sending the request and getting a valid server response.
+		// casesResponse := sftool.SendRequest(casesReq)
+
+		// // Parsing the JSON response.
+		// output := sftool.UnmarshalSF(casesResponse)
+
+		// // Printing the relevant info from the response.
+		// sftool.PrettyPrintBacklog(output)
 	default:
 		fmt.Println("sr - Service Request")
 		fmt.Println("lv - List View (placeholder)")
